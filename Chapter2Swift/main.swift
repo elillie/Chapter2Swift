5da03c4aa35fab35f3b0c36c4fa44fff923599c2
Ethan Lillie <ethan@smicrofarms.com>
2014-08-26 19:57:00 -0700

    Initial Commit

---
+++ Chapter2Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter2Swift
+//
+//  Created by Ethan Lillie on 8/26/14.
+//  Copyright (c) 2014 Algorithms. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
